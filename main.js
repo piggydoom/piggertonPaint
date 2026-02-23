61bce15919f7edca5c8ea114f993f2e11dba2e7f
piggydoom <artistpiggy@gmail.com>
2026-02-24 09:13:52 +1100

    wip colour picker

---
+++ main.js
@@ -0,0 +1,36 @@
+const colourChange = document.getElementById("colourWheel");
+const colourSelWindow = document.getElementById("colourSel");
+// let colourSelComputedStyle = window.getComputedStyle(colourSelWindow);  
+// const colourSelWindowDisplay = colourSelComputedStyle.display;
+// let colourSelDisplay = window.getComputedStyle(colourSelWindow).display;
+let colourSelDisplay = "none";
+
+colourChange.onload = console.log("test");
+
+// function openColourSel(){
+//  colourSelWindow.style.display = "flex";
+// };
+
+function openColourPicker(){
+ console.log("open");
+ openColourSel();
+};
+
+// function closeColourPicker(){
+// colourSelWindow.style.display = "none";
+// };
+
+function changeStateColourPicker(){
+if(colourSelDisplay === "none"){
+     colourSelWindow.style.display = "flex";
+     console.log("changeState init, determined to be none and attempted to show.");
+     colourSelDisplay = "flex";
+} else if(colourSelDisplay === "flex"){
+colourSelWindow.style.display = "none";
+console.log("changeState init, determined to be show and attempted to hide.");
+let colourSelDisplay = "none";
+};
+
+};
+
+colourChange.addEventListener("click", changeStateColourPicker)
