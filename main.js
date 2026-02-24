d57ff735af954c6e23e9299befe458d030d0912b
piggydoom <artistpiggy@gmail.com>
2026-02-24 14:20:19 +1100

    wip colour preview

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 const colourChange = document.getElementById("colourWheel");
 const colourSelWindow = document.getElementById("colourSel");
-// let colourSelComputedStyle = window.getComputedStyle(colourSelWindow);  
-// const colourSelWindowDisplay = colourSelComputedStyle.display;
-// let colourSelDisplay = window.getComputedStyle(colourSelWindow).display;
+const headerCanvas = document.getElementById("headerCanvas");
+const ctx = headerCanvas.getContext('2d');
+const RGB1 = document.getElementById('RGB1');
+const RGB2 = document.getElementById('RGB2');
+const RGB3 = document.getElementById('RGB3');
 let colourSelDisplay = "none";
+
 
 colourChange.onload = console.log("test");
 
@@ -20,17 +23,39 @@ function openColourPicker(){
 // colourSelWindow.style.display = "none";
 // };
 
+// function resizeCanvas(){
+// screen.width = window.innerWidth;
+// screen.height = window.innerHeight;
+// };
+
 function changeStateColourPicker(){
 if(colourSelDisplay === "none"){
      colourSelWindow.style.display = "flex";
      console.log("changeState init, determined to be none and attempted to show.");
      colourSelDisplay = "flex";
+     RGB1.style.display = "inline";
+     RGB2.style.display = "inline";
+     RGB3.style.display = "inline";
+     ctx.fillRect((RGB3.style.x + 4), (RGB3.style.y), 10, 10);
 } else{
 colourSelWindow.style.display = "none";
 console.log("changeState init, determined to be show and attempted to hide.");
 colourSelDisplay = "none";
+RGB1.style.display = "none";
+RGB2.style.display = "none";
+RGB3.style.display = "none";
+};
 };
 
-};
 
-colourChange.addEventListener("click", changeStateColourPicker)
+
+colourChange.addEventListener("click", changeStateColourPicker);
+
+
+function resizeCanvas() {
+  headerCanvas.width = canvas.offsetWidth;
+  headerCanvas.height = canvas.offsetHeight;
+}
+
+resizeCanvas();
+window.addEventListener("resize", resizeCanvas);
