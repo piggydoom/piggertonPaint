7ad8424d1a4b31a09c651a2d03cbbf7a0957ae00
piggydoom <artistpiggy@gmail.com>
2026-03-20 14:04:28 +1100

    finished colour dialog box

--- buttonsAndControls.js
+++ buttonsAndControls.js
@@ -17,15 +17,13 @@ const optionWindowElementsArray = Array.from(document.querySelector(".optionWind
 
 const hueSlider = document.getElementById('hueSlider');
 
-// let R = RGB1.value;
-// let G = RGB2.value;
-// let B = RGB3.value;
-
 let thickness = thicknessVal.value;
 let colourSelDisplay = "none";
 let thicknessSelDisplay = "none";
 let fillShapeSelDisplay = "none";
 let CSBhue = 0;
+let saturationValue = 100;
+let lumenValue = 68;
 
 var pixelData;
 var SBCPixelPosX;
@@ -43,16 +41,9 @@ window.onload = clearInputField("RGB2", 90);
 window.onload = clearInputField("RGB3", 90);
 
 window.onload = clearInputField("hueSlider", 0);
-// colourSelWindowChange();
 
 window.onload = clearInputField("thicknessVal", 15);
 thicknessUpdate();
-
-//COLOUR SEL
-// function openColourPicker() {
-//      console.log("open");
-//      openColourSel();
-// };
 
 function hideAllandShow(show, showDisplay){
      optionWindowElementsArray.forEach(item => {
@@ -61,46 +52,28 @@ function hideAllandShow(show, showDisplay){
     });
 };
 
-// function colourSelWindowChange() {
-//      R = RGB1.value;
-//      G = RGB2.value;
-//      B = RGB3.value;
-//      ctx.strokeStyle = 'rgb(' + R + ',' + G + ',' + B + ')';
-//      ctx.fillStyle = 'rgb(' + R + ',' + G + ',' + B + ')';
-//      ctxOver.strokeStyle = 'rgb(' + R + ',' + G + ',' + B + ')';
-//      colourSelWindow.style.backgroundColor = 'rgb(' + R + ',' + G + ',' + B + ')';
-// };
-
 function thicknessUpdate() {
      thickness = thicknessVal.value;
      ctx.lineWidth = thickness;
      ctxOver.lineWidth = thickness;
 };
 
+
 function resetLinearGradientCSB(){
-     //reset
-     // ctxSelectionBox.fillStyle = 'rgba(0,0,0,0)'
-     // ctxSelectionBox.fillRect(0, 0, selectionBoxCanvas.width, selectionBoxCanvas.height);
-     //let i = 0; i < (selectionBoxCanvas.width / 5) * (selectionBoxCanvas.height / 5); i++ for loop
-     ctxSelectionBox.moveTo(selectionBoxCanvas.width, 0);
      SBCPixelPosX = 0;
      SBCPixelPosY = 0;
      CSBsaturation = 0;
      let SBCBaseLumen = 100;
+     CSBluminescence = SBCBaseLumen;
 
      while(SBCPixelPosY <= selectionBoxCanvas.height){
           
-          if(SBCPixelPosX < selectionBoxCanvas.width){
-          // x
-          // console.log(ctxSelectionBox.fillStyle = 'HSL(' + CSBhue + ',' + CSBsaturation + '%,' + CSBluminescence +'%)');
-          // console.log(SBCPixelPosX + " " + SBCPixelPosY);   
+          if(SBCPixelPosX < selectionBoxCanvas.width){ 
           ctxSelectionBox.fillStyle = 'HSL(' + CSBhue + ',' + CSBsaturation + '%,' + CSBluminescence +'%)';
           ctxSelectionBox.fillRect(SBCPixelPosX, SBCPixelPosY, 5, 5);
           SBCPixelPosX = SBCPixelPosX + 5;
           CSBsaturation = CSBsaturation + (100 / 60);
           CSBluminescence = CSBluminescence - (50 / 60);
-          
-          // console.log("irow=" + iRow);
           } else if(SBCPixelPosX >= selectionBoxCanvas.width){
                SBCPixelPosX = 0;
                SBCPixelPosY = SBCPixelPosY + 5;
@@ -117,31 +90,13 @@ window.onload = () => {
      resetLinearGradientCSB();
 };
 
-//eventlisteners
-
-// RGB1.addEventListener('input', colourSelWindowChange);
-// RGB2.addEventListener('input', colourSelWindowChange);
-// RGB3.addEventListener('input', colourSelWindowChange);
 
 thicknessVal.addEventListener('input', thicknessUpdate);
 fillShape.addEventListener('input', () => {fillShapeToggle = fillShape.checked; console.log(fillShapeToggle)});
 
-// colourChange.addEventListener("click", () => {
-//      hideAllandShow(colourSelWindow, "inline");
-//           colourSelWindow.style.display = "inline";
-//           RGB1.style.display = "inline";
-//           RGB2.style.display = "inline";
-//           RGB3.style.display = "inline";
-// });
-
 hueSlider.addEventListener('input', () => {
-// console.log("hue-rotate(" + hueSlider.value + "deg)");
 CSBhue = hueSlider.value;
 resetLinearGradientCSB()
-
-// findMouseXColourSel(selectionBoxCanvas, evt);
-// findMouseYColourSel(selectionBoxCanvas, evt);
-console.log(selectedX);
 });
 
 thicknessChange.addEventListener("click", () => {
@@ -187,20 +142,15 @@ selectionBoxCanvas.addEventListener('mousedown', (evt) => {
      console.log("y= " + selectedY);
      console.log("x= " + selectedX);
 
-     pixelData = ctxSelectionBox.getImageData(selectedX, selectedY, 1, 1).data;
+     saturationValue = selectedX / 1.2;
+     lumenValue = (100 - selectedX / 1.2 / 2 - selectedY / 1.2);
 
-     const R = pixelData[0];   // Red component
-     const G = pixelData[1]; // Green component
-     const B = pixelData[2];  // Blue component
-     const A = pixelData[3]; // Alpha component (transparency)
+     console.log(saturationValue);
 
-     //find the RGB values based upon the percentage across the field the selected x/y is and matching that to the percentage of 255 to fix the saturation but still get the colour from getimagedata
-     saturationMultiplyFactor = 1+ selectedY / 120;
-     console.log(saturationMultiplyFactor);
-
-     ctx.strokeStyle = 'rgb(' + R + ',' + G + ',' + B + ')';
-     ctxOver.fillStyle = 'rgb(' + R + ',' + G + ',' + B +')';
-     console.log('rgb(' + R + ',' + G + ',' + B +')');
+     ctx.strokeStyle = 'HSL(' + CSBhue + ',' + saturationValue + '%,' + lumenValue +'%)';
+     ctxOver.fillStyle = 'HSL(' + CSBhue + ',' + saturationValue + '%,' + lumenValue +'%)';
+     ctxOver.strokeStyle = 'HSL(' + CSBhue + ',' + saturationValue + '%,' + lumenValue +'%)';
+     // console.log('HSL(' + CSBhue + ',' + saturationValue + '%,' + lumenValue +'%)');
      ctxOver.fillRect(50, 50, 40, 40);
 });
 
